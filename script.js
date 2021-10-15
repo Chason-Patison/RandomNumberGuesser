6d9bbc4ed82efce192ddf81df78e9ea8c5cdb5c1
Chason-Patison <91712943+Chason-Patison@users.noreply.github.com>
2021-10-15 12:26:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    Math.floor(Math.random() * 9);
+    return Math.floor(Math.random() * 9);
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    Math.abs(targetGuess - humanGuess)
+    if (Math.abs((targetGuess - humanGuess)) > (Math.abs(targetGuess - computerGuess))) {
+      return false;
+    } else if (humanGuess === computerGuess) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+  };
+  const advanceRound = () => {
+      currentRoundNumber += 1;
+  };
